495954c9ba7ed21a17414801cf5bd4d1b5efe117
Mario Ravalli <mario@raval.li>
2022-05-06 22:31:14 +0200

    first commit

---
+++ elixir/language-list/lib/language_list.ex
@@ -0,0 +1,25 @@
+defmodule LanguageList do
+  def new() do
+    # Please implement the new/0 function
+  end
+
+  def add(list, language) do
+    # Please implement the add/2 function
+  end
+
+  def remove(list) do
+    # Please implement the remove/1 function
+  end
+
+  def first(list) do
+    # Please implement the first/1 function
+  end
+
+  def count(list) do
+    # Please implement the count/1 function
+  end
+
+  def functional_list?(list) do
+    # Please implement the functional_list?/1 function
+  end
+end
