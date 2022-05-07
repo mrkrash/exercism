0ce58bc05ea617943a264e6428135811251e92fa
Mario Ravalli <mario@raval.li>
2022-05-07 22:51:46 +0200

    first iteration

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,25 @@
 defmodule LanguageList do
   def new() do
-    # Please implement the new/0 function
+    []
   end
 
   def add(list, language) do
-    # Please implement the add/2 function
+    [language] ++ list
   end
 
   def remove(list) do
-    # Please implement the remove/1 function
+    tl(list)
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    hd(list)
   end
 
   def count(list) do
-    # Please implement the count/1 function
+    length(list)
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    "Elixir" in list
   end
 end
