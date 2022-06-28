970dad366f8ad57fa6de02e2461fb1785b01ea54
Mario Ravalli <mario@raval.li>
2022-06-28 14:48:13 +0200

    HighScore second iteration

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,21 +1,15 @@
 defmodule HighScore do
+  @initial_score 0
+
   def new(), do: %{}
 
-  def add_player(scores, name, score \\ 0), do: Map.put(scores, name, score)
+  def add_player(scores, name, score \\ @initial_score), do: Map.put(scores, name, score)
 
   def remove_player(scores, name), do: Map.delete(scores, name)
 
-  def reset_score(scores, name), do: add_player(remove_player(scores, name), name)
+  def reset_score(scores, name), do: Map.put(scores, name, @initial_score)
 
-  def update_score(scores, name, score) do
-    if (scores[name] == nil) do
-        add_player(remove_player(scores, name), name, score)
-      else
-        add_player(remove_player(scores, name), name, score + scores[name])
-    end
-  end
+  def update_score(scores, name, score), do: Map.update(scores, name, score, fn old_score -> old_score + score end)
 
-  def get_players(scores) do
-    Map.keys(scores)
-  end
+  def get_players(scores), do: Map.keys(scores)
 end
